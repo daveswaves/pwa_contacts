7ffba4430385bbb0927381d85b0d01a9f56e9cfe
David <daveswaves.2008@gmail.com>
2024-07-01 19:42:16 +0100

    Init commit

---
+++ js/common.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // nav menu
+  const menus = document.querySelectorAll('.side-menu');
+  M.Sidenav.init(menus, {edge: 'left'});
+  // add contacts form
+  const forms = document.querySelectorAll('.side-form');
+  M.Sidenav.init(forms, {edge: 'right'});
+});
